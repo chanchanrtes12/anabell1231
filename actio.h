30b3d820cb492ddc7d514b38359a14295224bf3f
chanchanrtes12 <150329133+chanchanrtes12@users.noreply.github.com>
2023-12-06 21:02:25 +0900

    Update and rename action.h to actio.h

--- actio.h
+++ actio.h
@@ -4,14 +4,19 @@
 #include "collision.h"
 #include "item.h"
 #include "player.h"
+#include "main.h"
+#include "gravity.h"
+#include "draw.h"
+#include "item.h"
 extern char score_text[BUFSIZE];
 extern int score;
-void ActGame(int i);
+void ActGame();
 void ActGameOver(void);
 void ActCheckgoal1(void);
 void ActCheckgoal2(void);
-void ActPlayer(int i);
-void ActPlayer1(int i);
+void ActPlayer1(void);
+void ActPlayer2(void);
 void ActScoreBoard(void);
 void ActFinalScoreBoard(void);
 void ActGameOverScreen(void);
+void itemtotal(void);
