510da76f4804e86b95d17ae9448d718bdbb6ad8d
chanchanrtes12 <150329133+chanchanrtes12@users.noreply.github.com>
2023-12-06 21:05:28 +0900

    Update gravity.c

--- gravity.c
+++ gravity.c
@@ -1,6 +1,7 @@
 #include "gravity.h"
 
-void gravity(int i){
+void gravity(){
     BALL.v_y-=BALL.v_y-5;
-    player[i].v_y=-5;
+    Character[0].player[a[num0]].v_y-=5;
+    Character[1].player[a[num1]].v_y-=5;
 }
