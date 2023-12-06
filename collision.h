53f32a476831ced3428573836778f0003fe5c04b
chanchanrtes12 <150329133+chanchanrtes12@users.noreply.github.com>
2023-12-06 14:59:39 +0900

    Create collision.h

---
+++ collision.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include "utils.h"
+#include "gravity.h"
+#include "action.h"
+#include "item.h"
+#include "utils.h"
+#include "player.h"
+
+void elasticCollision2D(Entity *object1, Entity *object2);
+
+void Act(void);
