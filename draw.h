d13b66901c73a6b82befeaba82f137b007a90394
chanchanrtes12 <150329133+chanchanrtes12@users.noreply.github.com>
2023-12-06 15:02:08 +0900

    Create draw.h

---
+++ draw.h
@@ -0,0 +1,40 @@
+
+#ifndef DRAW_H
+#define DRAW_H
+
+#include "defs.h"
+#include "main.h"
+#include "init.h"
+
+extern Application application;
+
+
+void ClearWindow(void);
+
+void ShowWindow(void);
+
+void DrawStart(void);
+
+void DrawChoice(void);
+
+void DrawGameSetting(void);
+
+void DrawGame3(void);
+
+void DrawGame5(void);
+
+void DrawGame7(void);
+
+void DrawGame9(void);
+
+void Draw1pWin(void);
+
+void Draw2pWin(void);
+
+
+
+
+
+
+
+#endif
