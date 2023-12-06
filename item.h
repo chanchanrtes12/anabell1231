32ea62ccd4b0ef431eabe33ad6e9ba24f2564914
chanchanrtes12 <150329133+chanchanrtes12@users.noreply.github.com>
2023-12-06 14:56:34 +0900

    Create item.h

---
+++ item.h
@@ -0,0 +1,15 @@
+#define ITEM_WIDTH 20
+#define ITEM_HEIGHT 20
+#include <stdlib.h>
+#include <time.h>
+#include "main.h"
+#include "defs.h"
+#include "utils.h"
+void initializeItem();
+int checkitemFunctionOne();
+void itemFunctionOne();
+int checkitemFunctionTwo();
+void itemFunctionTwo();
+int checkitemFunctionThree();
+void itemFunctionThree();
+void initializeItem();
