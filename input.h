45625d8702e3ea32214663fd247cea6aaf3d1703
chanchanrtes12 <150329133+chanchanrtes12@users.noreply.github.com>
2023-12-06 10:22:08 +0900

    Create input.h

---
+++ input.h
@@ -0,0 +1,31 @@
+#ifndef INPUT_H
+#define INPUT_H
+
+
+#include "defs.h"
+#include "main.h"
+#include "init.h"
+#include "draw.h"
+
+
+/* main.h에 정의된 전역 변수 사용 */
+extern Application application;
+extern Entity player[3];
+extern Entity BALL;
+extern Entity game_over;
+
+
+
+void GetInput(void);
+
+
+void ResponseKeyUp(SDL_KeyboardEvent *event);
+
+
+void ResponseKeyDown(SDL_KeyboardEvent *event);
+
+void click1();
+void click2();
+void click3();
+
+#endif
