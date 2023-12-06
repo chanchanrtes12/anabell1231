32664e89a64085e8a44a529949f9c8af197fc476
chanchanrtes12 <150329133+chanchanrtes12@users.noreply.github.com>
2023-12-06 15:04:33 +0900

    Create init.h

---
+++ init.h
@@ -0,0 +1,21 @@
+
+#include "defs.h"
+
+void InitSDL(void);
+void InitTTF(void);
+void QuitSDL(int flag);
+void QuitTTF(void);
+void InitMemorySet(void);
+void InitScoreBoard(void);
+void Initfield(void);
+void InitPlayer1(void);
+void InitPlayer1_1(void);
+void InitPlayer2(void);
+void InitPlayer2_1(void);
+void InitPlayer3(void);
+void InitPlayer3_1(void);
+void InitLgoalnet(void);
+void InitRgoalnet(void);
+void InitBall(void);
+void InitGameOver(void);
+void initial_value(void);
